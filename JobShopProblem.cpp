2b13fa1042ff2986ed996c75e4419dec09b15d38
david <david.ben.freeman@gmail.com>
2025-09-28 16:49:11 +0200

    jobshop mutate

--- JobShopProblem.cpp
+++ JobShopProblem.cpp
@@ -70,3 +70,57 @@ bool JobShopProblem::StopCondition() const {
     return drought_count >= max_drought;
 }
 
+std::pair<ptrdiff_t, ptrdiff_t> CheckSwitchValidity(const int m_id, const int m_num, matrix<operation>& schedule, operation& op) {
+
+    ptrdiff_t prev_pos = -1;
+    ptrdiff_t next_pos = -1;
+    for (int i = 0; i < m_num; ++i) {
+        if (m_id == i) continue;
+        auto& r = schedule.row(i);
+        auto op_prev = std::find_if(r.cbegin(), r.cend(),
+            [op](const operation& prev) {
+                return op.job_id == prev.job_id && op.precedence - prev.precedence == 1;
+            });
+        auto op_next = std::find_if(r.cbegin(), r.cend(),
+            [op](const operation& next) {
+                return op.job_id == next.job_id && op.precedence + 1 == next.precedence;
+            });
+        if (op_prev != r.cend()) {
+            prev_pos = std::distance(r.cbegin(), op_prev);
+        }
+        if (op_next != r.cend()) {
+            next_pos = std::distance(r.cbegin(), op_next);
+        }
+        if (next_pos != -1 && prev_pos != -1) break;
+    }
+    return std::make_pair(prev_pos, next_pos);
+}
+
+matrix<operation> JobShopProblem::Mutate(matrix<operation> & schedule) {
+    std::random_device rnd;
+    std::mt19937 gen{rnd()};
+    bool switch_success = false;
+    operation* op1 = nullptr;
+    operation* op2 = nullptr;
+    while (!switch_success) {
+
+        std::uniform_int_distribution dist(0, schedule.row_num() - 1);
+
+        const auto machine = dist(gen);
+        auto& row = schedule.row(machine);
+        dist = std::uniform_int_distribution(0, static_cast<int>(row.size()) - 1);
+        const int op1_pos = dist(gen);
+        const int op2_pos = dist(gen);
+        op1 = &row[dist(gen)];
+        op2 = &row[dist(gen)];
+        auto [op1_prev_pos, op1_next_pos] = CheckSwitchValidity(machine, machines_num, schedule, *op1);
+        auto [op2_prev_pos, op2_next_pos] = CheckSwitchValidity(machine, machines_num, schedule, *op2);
+        if (!(op2_pos <= op1_prev_pos || op2_pos >= op1_next_pos) && !(op1_pos <= op2_prev_pos || op1_pos >= op2_next_pos)) {
+            switch_success = true;
+        }
+    }
+    const auto temp = *op1;
+    *op1 = *op2;
+    *op2 = temp;
+    return schedule;
+}
